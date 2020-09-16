bc42cc302ca95d235ab48e133df8877944b35de0
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-09-16 09:47:39 -0400

    done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value){
-    return  Object.assign({}, driver, { [key]: value })
+    let new_driver = Object.assign({}, driver, { [key]: value })
+    return  new_driver
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
@@ -12,7 +13,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 
 function deleteFromDriverByKey(driver, key){
-    let new_driver = {...driver} 
+    // let new_driver = {...driver} 
+    let new_driver = Object.assign( {}, driver )
     delete new_driver[key]
     return new_driver
 }
